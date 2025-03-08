1bf1c719ce2619bdd2171afee44a56eb8fbbee15
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-08 09:08:25 -0300

    feat(model-controller): create model and controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -24,5 +25,7 @@ class DatabaseSeeder extends Seeder
             'permissions' => 'admin',
 
         ]);
+
+        Product::factory()->count(10)->create();
     }
 }
